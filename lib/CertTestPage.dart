642c3fc39b37dddf884f1f7e7a4b36749452384f
Will Hammill <20711987@students.latrobe.edu.au>
2023-09-04 14:10:58 +1000

    Modified xml files and added to dart

--- lib/CertTestPage.dart
+++ lib/CertTestPage.dart
@@ -29,10 +29,108 @@ class MyApp extends StatelessWidget {
 class CertTestScreen extends StatelessWidget {
   const CertTestScreen({Key? key}) : super(key: key);
   static const String _title = 'IWipe';
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      // Add a MaterialApp here as well
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('IWipe'),
+            backgroundColor: Color(0xffB8E28A),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'This is your certification quiz, to pass you must have a score of atleast 50% ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                ProgressIndicator(
+
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Question 1: ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Answer 1'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('answer 2'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Answer 3'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Next Question'),
+                ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/IDSelectScreen');
+                    },
+                    child: const Text('Payment'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '');
+                    },
+                    child: const Text('Progress'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/IDSelectScreen');
+                    },
+                    child: const Text('Home'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '');
+                    },
+                    child: const Text('Notifications'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '');
+                    },
+                    child: const Text('Settings'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/ModuleTemplate.dart
+++ lib/ModuleTemplate.dart
@@ -6,6 +6,10 @@ import 'package:iwipe/main.dart';
 
 
 void main() async {
+ // runApp(DevicePreview(
+  //  enabled: true,
+   // builder: (context) => const MyApp(),
+  //));
   runApp(const MyApp());
 }
 
@@ -30,9 +34,85 @@ class ModuleTemplate extends StatelessWidget {
   const ModuleTemplate({Key? key}) : super(key: key);
   static const String _title = 'IWipe';
 
+  get image => null;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      // Add a MaterialApp here as well
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Living room'),
+            backgroundColor: Color(0xffB8E28A),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                const Image(
+                  image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Living room progress:',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'To see more, please click a button',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/IDSelectScreen');
+                  },
+                  child: const Text('Payment'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Progress'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/IDSelectScreen');
+                  },
+                  child: const Text('Home'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Notifications'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Settings'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
