cf660f77ce0fd98922d7573275a2bd7b0594a16a
bgspa <brandenjgoodman@gmail.com>
2023-09-05 22:40:00 +1000

    Missing from previous commit

---
+++ lib/ModuleTemplate.dart
@@ -0,0 +1,167 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+
+
+
+void main() async {
+ // runApp(DevicePreview(
+  //  enabled: true,
+   // builder: (context) => const MyApp(),
+  //));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'IWipe';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      initialRoute: '/', // Specify the initial route
+      routes: {
+        '/': (context) => ModuleTemplate(),
+        //'/SignIn': (context) => SignIn(),
+      },
+    );
+  }
+}
+
+class ModuleTemplate extends StatelessWidget {
+  const ModuleTemplate({Key? key}) : super(key: key);
+  static const String _title = 'IWipe';
+
+  get image => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Add a MaterialApp here as well
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Living room'),
+            backgroundColor: Color(0xffB8E28A),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                const Image(
+                  image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Cleaning 1:',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                LinearPercentIndicator(
+                  width: 140.0,
+                  lineHeight: 14.0,
+                  percent: 0.5,
+                  backgroundColor: Colors.grey,
+                  progressColor: Colors.blue,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Cleaning 2:',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                LinearPercentIndicator(
+                  width: 140.0,
+                  lineHeight: 14.0,
+                  percent: 0.5,
+                  backgroundColor: Colors.grey,
+                  progressColor: Colors.green,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Cleaning 3:',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                LinearPercentIndicator(
+                  width: 140.0,
+                  lineHeight: 14.0,
+                  percent: 0.5,
+                  backgroundColor: Colors.grey,
+                  progressColor: Colors.green,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'To see more, please click a button',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                const Image(
+                  image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/IDSelectScreen');
+                  },
+                  child: const Text('Payment'),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Progress'),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/IDSelectScreen');
+                  },
+                  child: const Text('Home'),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Notifications'),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: const Text('Settings'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/SignUp.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+import 'SignIn.dart';
+import 'main.dart';
+
+  void main() async {
+    runApp(const SignUp());
+  }
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+  static const String _title = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      routes: {
+        '/home': (context) => const HomeScreen(),
+        '/SignIn': (context) => const SignIn(),
+        '/SignUp': (context) => const SignUp(),
+      },
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text(_title),
+                backgroundColor: Color(0xffB8E28A),
+        ),
+        body: const MySatefulWidget(),
+      ),
+    );
+  }
+}
+class MySatefulWidget extends StatefulWidget {
+ const MySatefulWidget({Key? key}) : super(key: key);
+
+ @override
+  State<MySatefulWidget> createState() => _MyStatefulWidget();
+}
+
+class _MyStatefulWidget  extends State<MySatefulWidget>{
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerLeft,
+            child: TextButton(
+              child: const Text('<- Back', style: TextStyle(fontSize: 15)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+            child: const Text(
+              'Create new Account',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+                fontSize: 30),
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Alredy Registered? Log in'),
+              TextButton(
+                child: const Text(
+                    'here',
+                    style: TextStyle(fontSize: 15)
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/SignIn');
+                },
+              )
+            ],
+          ),
+        Container(
+        padding: const EdgeInsets.fromLTRB(30, 30, 0, 0),
+        child: const Text(
+          'Name',
+          style: TextStyle(fontSize: 20),
+        )),
+          Container(
+              padding: const EdgeInsets.fromLTRB(30, 0, 40, 10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xffFEEAF0),
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              )
+          ),
+          Container(
+              padding: const EdgeInsets.fromLTRB(30, 30, 0, 0),
+              child: const Text(
+                'Email',
+                style: TextStyle(fontSize: 20),
+              )),
+          Container(
+              padding: const EdgeInsets.fromLTRB(30, 0, 40, 10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xffFEEAF0),
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              )
+          ),
+          Container(
+              padding: const EdgeInsets.fromLTRB(30, 30, 0, 0),
+              child: const Text(
+                'Password',
+                style: TextStyle(fontSize:20),
+              )
+          ),
+          Container(
+              padding: const EdgeInsets.fromLTRB(30, 0, 40, 40),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xffFEEAF0),
+                  border: OutlineInputBorder(),
+                  labelText: '******',
+                ),
+              )
+          ),
+          Container (
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(30, 0, 40, 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xffF490A5), // Background color
+                ),
+                child: const Text('Sign Up'),
+                onPressed: (){
+                  print(nameController.text);
+                  print(passwordController.text);
+                },
+              )
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(50),
+            child: const Text(
+              'Iwipe',
+              style: TextStyle(
+                fontSize:50,
+                color: Color(0xff62995A),
+              ),
+            ),
+          ),
+        ]
+      ));
+  }
+}
