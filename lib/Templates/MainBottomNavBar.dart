0b36a88f8234207becc3d22bcac6b4728cceead8
Will Hammill <20711987@students.latrobe.edu.au>
2023-09-25 13:41:36 +1000

    Added buttons to nav bar code

--- lib/Templates/MainBottomNavBar.dart
+++ lib/Templates/MainBottomNavBar.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 class MainBottomNavBar extends StatelessWidget {
 
-
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
@@ -13,9 +12,60 @@ class MainBottomNavBar extends StatelessWidget {
             icon: Icon(Icons.home)
 
           )
-        ]);
+        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                IconButton( // payment button
+                   iconSize: 72,
+                   icon: const Icon(Icons.wallet),
+                  onPressed: () {
+                     Navigator.pushNamed(context, '/paymentScreen');
+                     },
+                ),
+                IconButton( // progress button
+                  iconSize: 72,
+                  icon: const Icon(Icons.bar_chart),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/progressScreen');
+                    },
+                ),
+                IconButton( // home button
+                   iconSize: 72,
+                  icon: const Icon(Icons.my_location),
+                  onPressed: () {
+                     Navigator.pushNamed(context, '/homeScreen');
+                     },
+                ),
+                IconButton( // notification button
+                   iconSize: 72,
+                  icon: const Icon(Icons.notifications),
+                  onPressed: () {
+                     Navigator.pushNamed(context, '/NotificationScreen');
+                     },
+                ),
+                IconButton( // settings button
+                   iconSize: 72,
+                  icon: const Icon(Icons.settings),
+                  onPressed: () {
+                     Navigator.pushNamed(context, '/settingsScreen');
+                     },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+
+
   }
 }
+
 
 
 //To do
